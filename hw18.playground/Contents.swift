1ee2353fc02b5afde1519f2fd415052bde32a788
sbaranchuk <sbaranchuk@protonmail.com>
2023-05-26 00:12:03 +0300

    Creating and placing chips in storage(1-4)

--- hw18.playground/Contents.swift
+++ hw18.playground/Contents.swift
@@ -1,3 +1,77 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// MARK: - Structure Chip
+
+public struct Chip {
+    public enum ChipType: UInt32 {
+        case small = 1
+        case medium
+        case big
+    }
+
+    public let chipType: ChipType
+
+    public static func make() -> Chip {
+        guard let chipType = Chip.ChipType(rawValue: UInt32(arc4random_uniform(3) + 1)) else {
+            fatalError("Incorrect random value")
+        }
+
+        return Chip(chipType: chipType)
+    }
+
+    public func sodering() {
+        let soderingTime = chipType.rawValue
+        sleep(UInt32(soderingTime))
+    }
+}
+
+// MARK: - SpawningThread
+
+class SpawningThread: Thread {
+    private var timer = Timer()
+    private let storage: Storage
+
+    init(storage: Storage) {
+        self.storage = storage
+    }
+
+    override func main() {
+        timer = Timer.scheduledTimer(timeInterval: 2,
+                                     target: self,
+                                     selector: #selector(createChip),
+                                     userInfo: nil,
+                                     repeats: true)
+
+        RunLoop.current.add(timer, forMode: .common)
+        RunLoop.current.run(until: Date(timeIntervalSinceNow: 20))
+    }
+
+    @objc func createChip() {
+        print("чип создан")
+        storage.putInStorage(chip: Chip.make())
+    }
+}
+
+// MARK: - WorkingThread
+
+class WorkingThread: Thread {
+
+}
+
+// MARK: - Storage
+
+class Storage {
+    private var chipStorage = [Chip]()
+
+    func putInStorage(chip: Chip) {
+        chipStorage.append(chip)
+        print("чип добавлен в хранилище")
+    }
+}
+
+// MARK: - Start work
+
+let storage = Storage()
+let spawningThread = SpawningThread(storage: storage)
+
+spawningThread.start()
